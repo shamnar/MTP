d3cf2104190d5046fb14fbe68a44e1555d7d6829
Shamnar <shamnarp@gmail.com>
2019-12-04 14:44:04 +0530

    diff matrix implemented

--- Pyhton/diffMatrix.py
+++ Pyhton/diffMatrix.py
@@ -9,14 +9,49 @@ import cv2
 import numpy as np
 import math
 import sys
+from os import listdir
+from os.path import isfile, join
 ###################Image folder ##########################################
-input_folder1="/home/user/Documents/MTP/Images/MyEnData/Trajectory"
+folder_settings=[
+["/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/img/MathData/stylusData",
+"/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/img/MathData/MyMathData",
+"/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/python/MTP/Pyhton/outfilesMath"],
+
+["/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/img/BenData/stylusData",
+"/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/img/BenData/MyBenData",
+"/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/python/MTP/Pyhton/outfilesBen"],
+
+["/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/img/EnData/StylusData",
+"/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/img/EnData/trajectory",
+"/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/python/MTP/Pyhton/outfilesEn"]
+]
+
+image_sizes_dict= dict([(5,125),(4,128),(3,129),(10,150),(15,150),(20,160),(30,150),(40,160),(50,150),(75,150)])
 ##########################################################################
 
-def euclidian_distance(list1, list2):
-    """Distance between two vectors."""
-    squares = [(p-q) ** 2 for p, q in zip(list1, list2)]
-    return sum(squares) ** .5
+def has_image(arr, grid_size):
+    if (np.count_nonzero(arr))==(grid_size*grid_size):
+        return 0
+    else:
+        return 1
+
+def generate_matrix(img_array, window_size):
+    [row, col] = img_array.shape
+    msize = row//window_size
+    matrix = np.zeros((msize,msize))
+    i=j=0
+    c_index=r_index=0
+    while(i<row):
+        while(j<col):
+            window=img_array[i:i+window_size,j:j+window_size]
+            val=has_image(window, window_size)
+            matrix[r_index, c_index] = val
+            c_index+=1
+            j+=window_size
+        r_index+=1
+        i+=window_size
+    return(matrix)
+
 
 def draw_box(img_name):
     img_bw=img_name
@@ -104,40 +139,72 @@ def draw_box(img_name):
     new_arr=new_arr[top:bottom+1,:].T
     return (new_arr)
 
-img=cv2.imread(input_folder1 + "/1.png",0)
+def process_images(image1, image2, img_size, win_size):
+    img1=cv2.imread(image1,0)
+    img2=cv2.imread(image2,0)
 
-if (img is not None):
-    success=True
-else:
-    success=False
+    if (img1 is not None):
+        success=True
+    else:
+        success=False
 
-if (not success):
-    print ("Image reading Error")
-    sys.exit()
+    if (not success):
+        print ("Image reading Error..1")
+        sys.exit()
 
-#extract image portion and remove unwanted parts
-processed_img=draw_box(img)
-l1 = np.arange(4).reshape(2,2)
-print (l1)
-l2 = np.arange(4,8,1).reshape(2,2)
-print (l2)
-dist = np.linalg.norm(l1-l2)
-print (dist)
+    if (img2 is not None):
+        success=True
+    else:
+        success=False
 
-'''
-diff = euclidian_distance(l1,l2)
-print (diff)
+    if (not success):
+        print ("Image reading Error..2")
+        sys.exit()
 
-diff = euclidian_distance(l3,l4)
-print (diff)
-'''
+    processed_img1=draw_box(img1)
+    processed_img1 = cv2.resize(processed_img1,(img_size,img_size))
+    thresh, processed_img1 = cv2.threshold(processed_img1, 150, 255, cv2.THRESH_BINARY)
 
-#add 10 lines of margin
-[row,col]=processed_img.shape
+    processed_img2=draw_box(img2)
+    processed_img2 = cv2.resize(processed_img2,(img_size,img_size))
+    thresh, processed_img2 = cv2.threshold(processed_img2, 150, 255, cv2.THRESH_BINARY)
+
+    m1 = generate_matrix(processed_img1, win_size)
+    m2 = generate_matrix(processed_img2, win_size)
+    dist = np.linalg.norm(m1-m2)
+    return (dist)
+
+current_window_sizes=[10,15,20,30,40,50,75]
+current_lang=2
+for sizes in current_window_sizes:
+    g_img_size=image_sizes_dict[sizes]
+    output_folder=folder_settings[current_lang][2]
+    output_file=output_folder+"/win"+str(sizes)+".csv"
+    input_folder1=folder_settings[current_lang][0]
+    input_folder2=folder_settings[current_lang][1]
+    file_to_write = open(output_file, 'w') 
+    count=0
+    onlyfiles = [f for f in listdir(input_folder1) if isfile(join(input_folder1, f)) and f.endswith(".png")]
+    for el in onlyfiles:
+        start_str= el.split("_")
+        end_str = start_str[1].split(".")
+        substr=start_str[0]+"To"+end_str[0]+"F.png"
+        matching_files = [f for f in listdir(input_folder2) if isfile(join(input_folder2, f)) and f.endswith(substr)]
+        if (len(matching_files)>0):
+            count+=1
+            filename1=input_folder1+"/"+el
+            filename2=input_folder2+"/"+matching_files[0]
+            diffval=process_images(filename1, filename2, g_img_size, sizes)
+            str_combined=el+","+matching_files[0]+","+str(diffval)
+            print(str_combined, file = file_to_write)
+            print(count,diffval)
+#print (count)
+    file_to_write.close() 
 
 
-print (row,col)
-print (type(processed_img))
+
+
+
         
 
 
