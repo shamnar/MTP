787e77df0e5c0d184c452ac0f54c8086229124dc
Shamnar <shamnarp@gmail.com>
2020-03-19 17:03:33 +0530

    local

--- Pyhton/multiMethods.py
+++ Pyhton/multiMethods.py
@@ -13,12 +13,13 @@ from os import listdir
 from os.path import isfile, join
 from os import path
 ###################Image folder ##########################################
-stylus_data_common="/home/user/Documents/MTP/Images/1FreeFormResults/StylusDatas"
-traj_common="/home/user/Documents/MTP/Images/1FreeFormResults"
+stylus_data_common="/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/img/EnData/StylusData"
+traj_common="/Users/shamnarpgmail.com/Downloads/ExperimentData/FreeFormResults"
 methods=["MultiUserProcess","MultiUserProcess_GyroPen","MultiUserProcess_NaiveBaseline"]
 types=["MyBenData","MyData","MyMathBenData","MyMathEngData"]
 users=["u00","u01","u02","u03","u04","u05","u06","u07","u08","u09"]
-out_common="/home/user/Documents/MTP/Images/1FreeFormResults/Output"
+out_common="/Users/shamnarpgmail.com/Downloads/ExperimentData/FreeFormResults/New/Output"
+
 '''
 folder_settings=[
 ["/Users/shamnarpgmail.com/Downloads/ExperimentData/proj/img/MathData/stylusData",
@@ -97,121 +98,6 @@ def draw_box(image_name):
     cropped = image_name[top: bottom, left: right]
     return (cropped)
 
-
-def draw_box_old(img_name):
-    img_bw=img_name
-    i=0
-    prev_count=0
-    img_width=img_bw.shape[1]
-    hline_start=hline_end=0
-    max_hline_start=max_hline_end=0
-    diff=0
-    state=0 # start line found
-    
-    for row in img_bw:
-        current_count=np.count_nonzero(row)
-        if (current_count!=img_width and prev_count==img_width and state==0):
-            hline_start=i
-            state=1 
-    
-        if (current_count==img_width and prev_count==img_width and (i-hline_start)>10 and state==1):
-            hline_end=i-1
-            state=0
-
-            if ((hline_end-hline_start)>diff):
-                diff=(hline_end-hline_start)
-                max_hline_start=hline_start
-                max_hline_end=hline_end
-                
-        
-        prev_count=current_count
-        i+=1
-    
-    new_arr=img_bw[max_hline_start:max_hline_end+1,:].T
-    img_width=new_arr.shape[1]
-
-    state=0
-    i=-1
-    top=bottom=0
-    min_height=20
-    max_gap=10
-    for row in new_arr:
-        i+=1
-        current_count = np.count_nonzero(row)
-        if (state==0 and current_count!=img_width):
-            state=1
-            top=i
-            blnk_count=0
-            continue
-        
-        if (state==1 and current_count==img_width):
-            blnk_count+=1
-            if (blnk_count>max_gap):
-                state=0
-                top=0;
-                continue
-        
-        if (state==1 and current_count==img_width and blnk_count<max_gap):
-            bottom=i-1
-            if ((bottom-top)<min_height):
-                bottom=0
-                state=1
-            else:
-                state=2
-                blnk_count=0
-            continue    
-            
-        
-        if (state==2):
-            if (current_count==img_width):
-                blnk_count+=1
-                if (blnk_count>max_gap):
-                    state=3
-            else:
-                state=2;
-                bottom=i-1
-                
-        if (state==3):
-            break
-
-    new_arr=new_arr[top:bottom+1,:].T
-    return (new_arr)
-
-def process_images1(image1, image2, img_size, win_size):
-    img1=cv2.imread(image1,0)
-    img2=cv2.imread(image2,0)
-
-    if (img1 is not None):
-        success=True
-    else:
-        success=False
-
-    if (not success):
-        print ("Image reading Error..1")
-        sys.exit()
-
-    if (img2 is not None):
-        success=True
-    else:
-        success=False
-
-    if (not success):
-        print ("Image reading Error..2")
-        sys.exit()
-
-    processed_img1=draw_box(img1)
-    processed_img1 = cv2.resize(processed_img1,(img_size,img_size))
-    thresh, processed_img1 = cv2.threshold(processed_img1, 200, 255, cv2.THRESH_BINARY)[1]
-
-    processed_img2=draw_box(img2)
-    processed_img2 = cv2.resize(processed_img2,(img_size,img_size))
-    thresh, processed_img2 = cv2.threshold(processed_img2, 127, 255, cv2.THRESH_BINARY)[1]
-
-    m1 = generate_matrix(processed_img1, win_size)
-    m2 = generate_matrix(processed_img2, win_size)
-    dist = np.linalg.norm(m1-m2)
-    return (dist)
-
 def draw_grids(image, grid_size, filename):
     [row,col]=image.shape
     points=list()
@@ -229,10 +115,10 @@ def get_only_filename(filename):
     return (f1[:-4])
 
 def process_images(image1, image2, diff_type):
-    print_flag=False
+    print_flag=True
     diff_list=[]
     win_sizes=[3,4,5,7,10,15,20,30,40,50]
-    print_windows=[3,5,7,10,30,40,50]
+    print_windows=[3,5,7,10,15,20,30,40,50]
     img_sizes_dict= dict([(3,129),(4,128),(5,125),(7,126),(10,150),(15,150),(20,160),(30,150),(40,160),(50,150),(75,150)])
     img1=cv2.imread(image1,cv2.IMREAD_UNCHANGED)
     img2=cv2.imread(image2,0)
@@ -295,8 +181,10 @@ def process_images(image1, image2, diff_type):
         if (print_flag):        
             extracted_filename1 = get_only_filename(image1)
             extracted_filename2 = get_only_filename(image2)
-            outf1="/home/user/Documents/MTP/Images/1FreeFormResults/Output/GridImages/Stylus/"
-            outf2="/home/user/Documents/MTP/Images/1FreeFormResults/Output/GridImages/Traj1/"
+            #outf1="/home/user/Documents/MTP/Images/1FreeFormResults/Output/GridImages/Stylus/"
+            #outf2="/home/user/Documents/MTP/Images/1FreeFormResults/Output/GridImages/Traj1/"
+            outf1="/Users/shamnarpgmail.com/Downloads/ExperimentData/Output/Grid_080220/"
+            outf2="/Users/shamnarpgmail.com/Downloads/ExperimentData/Output/Grid_080220/"
             extracted_filename1 = outf1 + extracted_filename1
             extracted_filename2 = outf2 + extracted_filename2
             if (size in print_windows):
@@ -304,26 +192,55 @@ def process_images(image1, image2, diff_type):
                 draw_grids(processed_img2, size, extracted_filename2)
             
     return (diff_list)
+'''
+index=3
+list_folder=["MultiUserProcess_FreeForm","MultiUserProcess_GyroPen_FreeForm","MultiUserProcess_NaiveBaseline_FreeForm","stylusData_FreeForm"]
+in_folder="/Users/shamnarpgmail.com/Downloads/ExperimentData/FreeFormResults/New/FreeFormResults"
+out_folder="/Users/shamnarpgmail.com/Downloads/ExperimentData/FreeFormResults/New/FreeFormResults/Resized_Images"
+input_folder = in_folder + "/" + list_folder[index]
+out_folder=out_folder +  "/" + list_folder[index] + "/"
+all_files = [f for f in listdir(input_folder) if isfile(join(input_folder, f)) and f.endswith(".png")]
+for s in all_files:
+    single_file = input_folder + "/" + s
+    print (single_file)
+    img_size=128
+    if (index==3):
+        img=cv2.imread(single_file,cv2.IMREAD_UNCHANGED)
+        *_,alpha=cv2.split(img)
+        img=cv2.bitwise_not(alpha)
+    else:
+        img=cv2.imread(single_file,0)
+
+    img=draw_box(img)
+    img = cv2.resize(img,(img_size,img_size))
+    fname=get_only_filename(single_file)
+    oimg_name=out_folder+fname + ".png"
+    print (oimg_name)
+    cv2.imwrite(oimg_name, img)
+'''
 
 
 
-for u in range(10):
-    user_index = u
-    process_index=2
-    input_folder = stylus_data_common + "/" + users[user_index]
+
+for u in range(1):
+    user_index = 0
+    process_index=0
+    input_folder = stylus_data_common
+    temp_common="/Users/shamnarpgmail.com/Downloads/ExperimentData/FreeFormResults"
     input_folder1 = traj_common + "/" + methods[process_index]+"/"+users[user_index]+"/"+types[0]
     input_folder2 = traj_common + "/" + methods[process_index]+"/"+users[user_index]+"/"+types[1]
     input_folder3 = traj_common + "/" + methods[process_index]+"/"+users[user_index]+"/"+types[2]
     input_folder4 = traj_common + "/" + methods[process_index]+"/"+users[user_index]+"/"+types[3]
-    output_file = out_common + "/" + methods[process_index] + "/"+users[user_index] + ".csv"
-    file_to_write = open(output_file, 'w') 
+    #output_file = out_common + "/" + methods[process_index] + "/"+users[user_index] + ".csv"
+    output_file = out_common  + "/"+methods[process_index] + ".csv"
+    #file_to_write = open(output_file, 'w') 
     count=0
 
-    onlyfiles = [f for f in listdir(input_folder) if isfile(join(input_folder, f)) and f.endswith(".png")]
+    onlyfiles = [f for f in listdir(input_folder) if isfile(join(input_folder, f)) and f.endswith("1569565744758_1569565751442.png")]
     for el in onlyfiles:
         start_str= el.split("_")
         end_str = start_str[1].split(".")
-        substr=start_str[0]+"To"+end_str[0]+".png"
+        substr=start_str[0]+"To"+end_str[0]+"F.png"
         matching_files1=[]
         matching_files2=[]
         matching_files3=[]
@@ -359,17 +276,44 @@ for u in range(10):
         if (found==1):
             count+=1
             filename1=input_folder+"/"+el
+            img_size=128
+            img=cv2.imread(filename1,cv2.IMREAD_UNCHANGED)
+            *_,alpha=cv2.split(img)
+            img=cv2.bitwise_not(alpha)
+            img1=cv2.imread(filename2,0)
+
+            outf="/Users/shamnarpgmail.com/Downloads/ExperimentData/Output/NoGrid_080220/"
+
+            img=draw_box(img)
+            img = cv2.resize(img,(img_size,img_size))
+            fname=get_only_filename(filename1)
+            oimg_name=outf+fname +".png"
+            print (oimg_name)
+            cv2.imwrite(oimg_name, img)
+
+            img1=draw_box(img1)
+            img1= cv2.resize(img1,(img_size,img_size))
+            fname=get_only_filename(filename2)
+            oimg_name=outf+fname +"_"+str(process_index) +".png"
+            print (oimg_name)
+            cv2.imwrite(oimg_name, img1)
+
+
+            '''
             difflist=process_images(filename1, filename2,1)        
             strlist= ', '.join(map(str,difflist))
             str_combined=el+","+el1+','+strlist
-            print(str_combined, file = file_to_write)
+            #print(str_combined, file = file_to_write)
             print(count,strlist)
+    
     if (count==133):
         print ("Ok" + str(user_index))
     else:
         print ("Not Ok" + str(user_index))
-    file_to_write.close() 
-
+    
+    print ("Done")
+    #file_to_write.close() 
+'''
 
 
 
